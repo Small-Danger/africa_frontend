458ac67058fea5a8116aef43900724607daec2dc
Small-Danger <landrybamabara04@gmail.com>
2025-09-04 21:42:40 +0100

    gestion des tailles des images 2

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,6 @@ import Banners from './pages/admin/Banners'
 import { CartProvider } from './contexts/CartContext'
 import { AuthProvider } from './contexts/AuthContext'
 import { NotificationProvider } from './contexts/NotificationContext'
-import WhatsAppButton from './components/WhatsAppButton'
 
 function App() {
   return (
@@ -190,8 +189,7 @@ function App() {
                 </ProtectedRoute>
               } />
             </Routes>
-            <WhatsAppButton />
-          </Router>
+           </Router>
         </CartProvider>
       </AuthProvider>
     </NotificationProvider>
